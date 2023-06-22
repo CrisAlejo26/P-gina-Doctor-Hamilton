7639d4d00dc9592ce8a0fb3ea8d4130a125783b8
Cristian Alejandro Arroayave <cristianalejo84@gmail.com>
2023-06-22 17:33:29 +0200

    first commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,22 @@
+
+export const Nav = () => {
+
+    return (
+        <>
+            <div>
+                
+            </div>
+            <div className="forms">
+                <nav>
+                    <h2>Hamilton</h2>
+                    <ul>
+                        <li>About</li>
+                        <li>Services</li>
+                        <li>Testimonials</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    )
+}
